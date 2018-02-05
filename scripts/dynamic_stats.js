da55ef0d8249ee18cff20edf21aab8a24344532b
Unknown <zhy.david@gmail.com>
2018-02-05 21:43:00 +0800

    improved dynamic stats

--- scripts/dynamic_stats.js
+++ scripts/dynamic_stats.js
@@ -1,12 +1,24 @@
 // jquery
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+$(window).on('scroll',function() {
+    var top_of_element = $(".count").offset().top;
+    var bottom_of_element = $(".count").offset().top + $(".count").outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+    var top_of_screen = $(window).scrollTop();
+
+    // if element is visible
+    if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        // show dynamic numbers
+        $('.count').each(function () {
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+        $(window).off('scroll'); // unlink scroll event
+    }
 });
